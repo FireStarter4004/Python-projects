d14f7409505378dbc32fc9a93a71975e4fd4c0a1
FireStarter4004 <ksb2000@outlook.com>
2025-12-10 15:44:49 -0500

    trying to fix  file io, having issues

--- To_do_list/button_commands.py
+++ To_do_list/button_commands.py
@@ -5,17 +5,19 @@ import json as js
 import To_do_list as tdl
 
 tasks=open("tasks.txt")
-listoftasks=tasks.readlines()
+listoftasks = tasks.readlines()
+tasks.close()
 
 
 def add_task(task_listbox, task_entry):
     task_text = task_entry.get()
-    if task_text:
-        tasks=open("tasks.txt","a")
-        tasks.write(task_text.strip("enter a new task  :") + "\n")
+    if task_text and task_text != "enter a new task :":
+        tasks = open("tasks.txt","a+")
+        tasks.write(task_text + "\n")
+        listoftasks = tasks.readlines()
         tasks.close()
         task_listbox.insert(tk.END, task_text)
 
         task_entry.delete(0, tk.END)
-        #task_entry.insert(0, "enter a new task  :")
+        task_entry.insert(0, "enter a new task  :")
   
